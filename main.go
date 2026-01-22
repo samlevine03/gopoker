ea2c0c0456472b2b9b6b2f5dab8ca941c9139747
Sam Levine <samlevine03@gmail.com>
2026-01-21 22:58:21 -0500

    Add board card support and pre-compiled Linux binary for Vercel

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gopoker/poker"
 	"os"
@@ -8,19 +9,49 @@ import (
 )
 
 func main() {
+	// Define -board flag for known board cards
+	boardFlag := flag.String("board", "", "Known board cards (e.g., 'Jh,Tc,9d' for flop or 'Jh,Tc,9d,8s' for flop+turn)")
+	flag.Parse()
+
+	args := flag.Args()
+
 	// Check if we have command-line arguments
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go As,Ks Qh,Qd [more hands...]")
-		fmt.Println("Example: go run main.go As,Ks Qh,Qd 6c,7c")
+	if len(args) < 2 {
+		fmt.Println("Usage: go run main.go [options] As,Ks Qh,Qd [more hands...]")
+		fmt.Println("Options:")
+		fmt.Println("  -board string  Known board cards (flop, flop+turn, or full board)")
+		fmt.Println("Example: go run main.go -board Jh,Tc,9d As,Ks Qh,Qd")
 		os.Exit(1)
+	}
+
+	// Parse board cards if provided
+	var knownBoard []uint32
+	usedCards := make(map[string]bool)
+
+	if *boardFlag != "" {
+		boardCardsStr := strings.Split(*boardFlag, ",")
+		if len(boardCardsStr) < 3 || len(boardCardsStr) > 5 {
+			fmt.Printf("Error: Board must have 3-5 cards (flop, flop+turn, or full board), got %d\n", len(boardCardsStr))
+			os.Exit(1)
+		}
+
+		knownBoard = make([]uint32, len(boardCardsStr))
+		for i, cardStr := range boardCardsStr {
+			cardStr = strings.TrimSpace(cardStr)
+			if usedCards[cardStr] {
+				fmt.Printf("Error: Duplicate card in board: %s\n", cardStr)
+				os.Exit(1)
+			}
+			usedCards[cardStr] = true
+			knownBoard[i] = poker.NewCard(cardStr)
+		}
 	}
 
 	// Parse hands from command-line arguments
 	var playerHands [][]uint32
-	usedCards := make(map[string]bool)
 
-	for i := 1; i < len(os.Args); i++ {
-		handStr := os.Args[i]
+	for i := 0; i < len(args); i++ {
+		handStr := args[i]
 		cards := strings.Split(handStr, ",")
 
 		if len(cards) != 2 {
@@ -54,7 +85,12 @@ func main() {
 	calculator := poker.NewCalculator()
 	calculator.SetDeck(deck)
 
-	playerEquities := calculator.CalculateEquity(playerHands)
+	var playerEquities []float64
+	if len(knownBoard) > 0 {
+		playerEquities = calculator.CalculateEquityWithBoard(playerHands, knownBoard)
+	} else {
+		playerEquities = calculator.CalculateEquity(playerHands)
+	}
 
 	for i := range playerHands {
 		fmt.Printf("Player %d: %.2f%%\n", i+1, playerEquities[i])
--- poker/calculator.go
+++ poker/calculator.go
@@ -105,3 +105,110 @@ func (c *Calculator) CalculateEquity(playerHands [][]uint32) []float64 {
 
 	return playerEquities
 }
+
+// CalculateEquityWithBoard calculates equity when some board cards are known
+// knownBoard can be 3 cards (flop), 4 cards (flop+turn), or 5 cards (full board)
+func (c *Calculator) CalculateEquityWithBoard(playerHands [][]uint32, knownBoard []uint32) []float64 {
+	// Remove player hands and known board cards from deck
+	for _, hand := range playerHands {
+		c.deck.RemoveCards(hand)
+	}
+	c.deck.RemoveCards(knownBoard)
+
+	remainingDeck := c.deck.cards
+	cardsNeeded := 5 - len(knownBoard)
+
+	var possibleBoards [][]uint32
+	if cardsNeeded == 0 {
+		// Full board is known, only one possible board
+		possibleBoards = [][]uint32{knownBoard}
+	} else {
+		// Generate combinations for unknown cards
+		unknownCardCombos := Combinations(remainingDeck, cardsNeeded)
+		possibleBoards = make([][]uint32, len(unknownCardCombos))
+		for i, unknownCards := range unknownCardCombos {
+			// Combine known board with unknown cards
+			board := make([]uint32, 5)
+			copy(board, knownBoard)
+			copy(board[len(knownBoard):], unknownCards)
+			possibleBoards[i] = board
+		}
+	}
+
+	numPlayers := len(playerHands)
+	totalBoards := len(possibleBoards)
+
+	// Set up a worker pool with goroutines
+	numWorkers := runtime.GOMAXPROCS(0)
+	boardChunks := chunkCombos(possibleBoards, numWorkers)
+
+	// Use a channel to collect results
+	results := make(chan [2][]int, totalBoards)
+
+	var wg sync.WaitGroup
+	wg.Add(len(boardChunks))
+
+	for _, boardChunk := range boardChunks {
+		go func(boardChunk [][]uint32) {
+			defer wg.Done()
+			localWins := make([]int, numPlayers)
+			localTies := make([]int, numPlayers)
+			scores := make([]int, numPlayers)
+
+			for _, board := range boardChunk {
+				bestScore := LookupTableMaxHighCard + 1
+				winners := 0
+
+				for i, hand := range playerHands {
+					scores[i] = c.evaluator.Evaluate(hand, board)
+					if scores[i] < bestScore {
+						bestScore = scores[i]
+						winners = 1
+					} else if scores[i] == bestScore {
+						winners++
+					}
+				}
+
+				if winners > 1 {
+					for i, score := range scores {
+						if score == bestScore {
+							localTies[i]++
+						}
+					}
+				} else {
+					for i, score := range scores {
+						if score == bestScore {
+							localWins[i]++
+							break
+						}
+					}
+				}
+			}
+			results <- [2][]int{localWins, localTies}
+		}(boardChunk)
+	}
+
+	// Close the results channel when all workers are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// Collect results from all workers
+	playerWins := make([]int, numPlayers)
+	playerTies := make([]int, numPlayers)
+	for res := range results {
+		for j := 0; j < numPlayers; j++ {
+			playerWins[j] += res[0][j]
+			playerTies[j] += res[1][j]
+		}
+	}
+
+	// Calculate equities
+	playerEquities := make([]float64, numPlayers)
+	for i := 0; i < numPlayers; i++ {
+		playerEquities[i] = (float64(playerWins[i]) + float64(playerTies[i])/float64(numPlayers)) / float64(totalBoards) * 100
+	}
+
+	return playerEquities
+}
