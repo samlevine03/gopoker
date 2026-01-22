381c79fd5c18029ad93f8a571caa31ffc7c908d8
Sam Levine <samlevine03@gmail.com>
2026-01-21 22:25:44 -0500

    Add CLI support for command-line arguments

--- main.go
+++ main.go
@@ -3,30 +3,60 @@ package main
 import (
 	"fmt"
 	"gopoker/poker"
-	"time"
+	"os"
+	"strings"
 )
 
 func main() {
-	playerHands := [][]uint32{
-		// TODO: once made into a CLI, need to make sure all input hands are unique
-		{poker.NewCard("As"), poker.NewCard("Ks")},
-		{poker.NewCard("Qh"), poker.NewCard("Qd")},
-		{poker.NewCard("6c"), poker.NewCard("7c")},
+	// Check if we have command-line arguments
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go As,Ks Qh,Qd [more hands...]")
+		fmt.Println("Example: go run main.go As,Ks Qh,Qd 6c,7c")
+		os.Exit(1)
+	}
+
+	// Parse hands from command-line arguments
+	var playerHands [][]uint32
+	usedCards := make(map[string]bool)
+
+	for i := 1; i < len(os.Args); i++ {
+		handStr := os.Args[i]
+		cards := strings.Split(handStr, ",")
+		
+		if len(cards) != 2 {
+			fmt.Printf("Error: Each hand must have exactly 2 cards, got %d in '%s'\n", len(cards), handStr)
+			os.Exit(1)
+		}
+
+		hand := make([]uint32, 2)
+		for j, cardStr := range cards {
+			cardStr = strings.TrimSpace(cardStr)
+			
+			// Check for duplicates
+			if usedCards[cardStr] {
+				fmt.Printf("Error: Duplicate card: %s\n", cardStr)
+				os.Exit(1)
+			}
+			usedCards[cardStr] = true
+			
+			hand[j] = poker.NewCard(cardStr)
+		}
+		
+		playerHands = append(playerHands, hand)
+	}
+
+	if len(playerHands) < 2 || len(playerHands) > 9 {
+		fmt.Printf("Error: Must have 2-9 players, got %d\n", len(playerHands))
+		os.Exit(1)
 	}
 
 	deck := poker.NewDeck(0)
 	calculator := poker.NewCalculator()
 	calculator.SetDeck(deck)
 
-	start := time.Now()
 	playerEquities := calculator.CalculateEquity(playerHands)
-	elapsed := time.Since(start)
 
-	for i, hand := range playerHands {
-		fmt.Printf("Player %d's Hand: ", i+1)
-		poker.PrintPrettyCards(hand)
-		fmt.Printf("\nPlayer %d's Preflop Equity: %.2f%%\n", i+1, playerEquities[i])
+	for i := range playerHands {
+		fmt.Printf("Player %d: %.2f%%\n", i+1, playerEquities[i])
 	}
-
-	fmt.Printf("\nCalculation took %s\n", elapsed)
 }
